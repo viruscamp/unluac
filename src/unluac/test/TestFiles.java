ee4687a12ca8cbda504b5ff2c4b2f824d3303a5a
tehtmi <devnull@localhost>
2022-01-06 23:43:48 -0800

    Test case for recent changes (auto-generated test case from John Fowler)

--- src/unluac/test/TestFiles.java
+++ src/unluac/test/TestFiles.java
@@ -208,6 +208,7 @@ public class TestFiles {
     new TestFile("break21"),
     new TestFile("break22", TestFile.DEFAULT_VERSION, TestFile.RELAXED_SCOPE), //TODO: relaxed scope is maybe not necessary if scopes inform hanger resolution?
     new TestFile("break23", TestFile.DEFAULT_VERSION, TestFile.RELAXED_SCOPE), //TODO: relaxed scope is maybe not necessary if scopes inform hanger resolution?
+    new TestFile("break24"),
     new TestFile("close01"),
     new TestFile("close02"),
     new TestFile("close03"),
---
+++ test/src/break24.lua
@@ -0,0 +1,16 @@
+local a
+while true do
+  if a then
+  else
+    local b,c
+    if c then
+      local f = function()
+      end
+      if b then
+      end
+      break
+    else
+      break
+    end
+  end
+end
